64d4f77be45aacd001c6522bc8f90d30224cacea
Lucas H B Guimaraes <lukahbg@gmail.com>
2025-08-19 13:02:29 -0300

    feat: add slider widget

---
+++ lib/ui/slider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class UISlider extends StatelessWidget {
+  final double value;
+  final ValueChanged<double> onChanged;
+  final double min;
+  final double max;
+  final bool vertical;
+
+  const UISlider({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    this.min = 0.0,
+    this.max = 1.0,
+    this.vertical = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final slider = SliderTheme(
+      data: Theme.of(context).sliderTheme,
+      child: Slider(
+        value: value,
+        onChanged: onChanged,
+        min: min,
+        max: max,
+      ),
+    );
+
+    if (vertical) {
+      return RotatedBox(
+        quarterTurns: 3,
+        child: slider,
+      );
+    }
+    return slider;
+  }
+}
